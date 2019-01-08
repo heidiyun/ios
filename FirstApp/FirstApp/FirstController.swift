aec943954cab3c78e7958c71b1627a388b77c013
Yunjeong Goo <heidiyun.goo@gmail.com>
2019-01-08 19:19:38 +0900

    FirstApp - 컴포넌트 연결 및 뷰 전환하기

---
+++ FirstApp/FirstApp/FirstController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class FirstController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+// 함수는 독립적,
+// 메소드는 객체 Context에 묶여있다.
+  
+
+}
